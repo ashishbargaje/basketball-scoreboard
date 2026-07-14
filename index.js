85b83f02daff4b5bb705f0e83f71795a6b5800cf
Ashish Vyanktesh Bargaje <abargaje@Ashishs-Mac-mini.local>
2026-07-14 22:39:50 +0530

    solo project as per the figma design + stretch goals (added 'New Game' button, 'Highlight the leader')

---
+++ index.js
@@ -0,0 +1,59 @@
+let homePointsCount = 0;
+
+function onePointHome(){
+    homePointsCount += 1;
+    document.getElementById("home-points").textContent = homePointsCount
+    leader()
+}
+
+function twoPointsHome(){
+    homePointsCount += 2;
+    document.getElementById("home-points").textContent = homePointsCount
+    leader()
+}
+
+function threePointsHome(){
+    homePointsCount += 3;
+    document.getElementById("home-points").textContent = homePointsCount
+    leader()
+}
+
+let guestPointsCount = 0;
+
+function onePointGuest(){
+    guestPointsCount += 1;
+    document.getElementById("guest-points").textContent = guestPointsCount
+    leader()
+}
+
+function twoPointsGuest(){
+    guestPointsCount += 2;
+    document.getElementById("guest-points").textContent = guestPointsCount
+    leader()
+}
+
+function threePointsGuest(){
+    guestPointsCount += 3;
+    document.getElementById("guest-points").textContent = guestPointsCount
+    leader()
+}
+
+function newGame(){
+    homePointsCount = 0
+    guestPointsCount = 0
+    document.getElementById("home-points").textContent = homePointsCount
+    document.getElementById("guest-points").textContent = guestPointsCount
+    document.getElementById("leader").textContent = "LEADER is "
+}
+
+function leader(){
+    if(homePointsCount > guestPointsCount){
+        document.getElementById("leader").textContent = "LEADER is Home"
+    }
+    else if(homePointsCount < guestPointsCount) {
+        document.getElementById("leader").textContent = "LEADER is Guest"
+    }
+    else{
+        document.getElementById("leader").textContent = "LEADER is None"
+    }
+}
